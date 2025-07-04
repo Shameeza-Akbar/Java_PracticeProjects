768da702a0152e846f3577539e479b0b6b1162a2
Shameeza Akbar <shameezaakbar@gmail.com>
2025-07-04 15:59:22 +0500

    First java course practice task

---
+++ Check_Number/src/PositiveNegativeZero.java
@@ -0,0 +1,6 @@
+public class PositiveNegativeZero {
+    // write code here
+    public static void checkNumber (int number){
+        System.out.print(number>0?"positive":(number<0?"negative":"zero"));
+    }
+}
